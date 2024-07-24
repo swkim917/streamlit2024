6f3e188103f0bc359d09b9512020836df7ac661d
swkim917 <swkim917@gmail.com>
2024-07-24 11:50:55 +0900

    dice

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import random
 
-st.write('Hello Streamlit!!')
-st.write('Hello, *World!* :sunglasses:')
-st.write('김상우')
+st.header('주사위 게임', divider='rainbow')
+isClicked = st.button('주사위던지기', type='primary')
+
+if isClicked :
+    n = random.randint(1, 6)
+    st.image(f'img/{n}.png')
